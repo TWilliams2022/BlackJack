8889960ce09f6b12a9e17f48176c5d44e0a7c3af
Melendez <henry.melendez@tlgcohort.com>
2022-11-14 08:28:55 -0800

    Added test methods for Hand

--- src/main/test/com/blackjackproject/cards/HandTest.java
+++ src/main/test/com/blackjackproject/cards/HandTest.java
@@ -1,21 +1,31 @@
 package com.blackjackproject.cards;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class HandTest {
-    Card sixHearts = new Card(Rank.SIX, Suit.Hearts);
-    Hand hand;
+    private Card sixHearts;
+    private BlackJackHand hand;
+
+    @Before
+    public void setUp() throws Exception {
+        sixHearts = new Card(Rank.SIX, Suit.Hearts);
+        hand = new BlackJackHand();
+    }
 
     @Test
     public void test_add_card() {
         hand.addCard(sixHearts);
-        System.out.println(hand);
+        assertEquals(1,hand.getCards().size());
+
     }
 
     @Test
     public void test_find_hand_value() {
+        hand.addCard(sixHearts);
+        assertEquals(6, hand.findHandValue());
 
     }
 }
