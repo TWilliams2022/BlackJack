2ccd26bd42f5bb2b8fb24cc454bfd91d3de59444
drod32 <drod32@gmail.com>
2022-11-14 17:01:58 -0500

    Added game logic

--- src/main/java/com/blackjackproject/BlackJackApplication.java
+++ src/main/java/com/blackjackproject/BlackJackApplication.java
@@ -1,7 +1,18 @@
 package com.blackjackproject;
 
+
+
 public class BlackJackApplication {
     public static void main(String[] args) {
+
+        BlackJackGame blackJackGame = new BlackJackGame();
+
+
+        blackJackGame.playGame();
+
+
+
+
 
 
 
